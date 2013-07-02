0f6994131e09fa6f8883caae51bdfc4f7c140494
yotchang <zh1bvtan1@gmail.com>
2013-07-02 06:26:46 +0100

    Add marmalade packages archive

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,8 @@
 ;; @ ELPA
 ;; ELPAの設定
 (when (require `package nil t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (setq package-user-dir (concat user-emacs-directory "elpa"))
   (package-initialize))
 

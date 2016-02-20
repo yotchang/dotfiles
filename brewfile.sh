c76edd8af9767102da6ad8fc9a3f61564aee74c7
yotchang4s <zh1bvtan1@gmail.com>
2016-02-20 20:32:10 +0900

    Change settings

--- brewfile.sh
+++ brewfile.sh
@@ -6,10 +6,10 @@ brew tap homebrew/binary || true
 brew tap homebrew/versions || true
 brew tap homebrew/dupes || true
 
+brew tap homebrew/php || true
+
 brew tap caskroom/cask || true
 brew tap caskroom/versions || true
-
-brew tap josegonzalez/homebrew-php || true
 
 brew install zsh || true
 brew install wget || true
@@ -33,18 +33,21 @@ brew install brew-cask || true
 
 brew cleanup || true
 
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
 brew cask install mi || true
-brew cask install caskroom/versions/java6
-brew cask install caskroom/versions/java7
-brew cask install caskroom/versions/java8
+brew cask install java6
+brew cask install java7
+brew cask install java
 brew cask install google-cloud-sdk || true
 brew cask install iterm2 || true
 brew cask install emacs || true
 brew cask install virtualbox || true
-brew cask install boot2docker || true
 brew cask install karabiner || true
 brew cask install intellij-idea || true
 brew cask install googleappengine || true
+brew cask install filezilla || true
+brew cask install docker-machine || true
 
 brew cask install cocoapacketanalyzer || true
 
@@ -59,5 +62,6 @@ brew cask install miro || true
 brew cask install slack || true
 brew cask install asepsis || true
 brew cask install kindle || true
+brew cask install fitbit-connect || true
 
 brew cask cleanup || true

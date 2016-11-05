3deaf5f23426c22ecfc70e09729e2255b36da562
yotchang4s <zh1bvtan1+github@gmail.com>
2016-11-05 13:52:13 +0900

    Change install package

--- brewfile.sh
+++ brewfile.sh
@@ -1,67 +1,68 @@
 #!/bin/sh
-brew update || true
-brew upgrade || true
+brew update
+brew upgrade
 
-brew tap homebrew/binary || true
-brew tap homebrew/versions || true
-brew tap homebrew/dupes || true
+brew tap homebrew/binary
+brew tap homebrew/versions
+brew tap homebrew/dupes
 
-brew tap homebrew/php || true
+brew tap homebrew/php
 
-brew tap caskroom/cask || true
-brew tap caskroom/versions || true
+brew tap caskroom/cask
+brew tap caskroom/versions
 
-brew install zsh || true
-brew install wget || true
-brew install curl || true
-brew install git || true
-brew install python3 || true
-brew install openssl || true
-brew install readline || true
-brew install rbenv || true
-brew install ruby-build || true
-brew install php55 || true
-brew install groovy || true
-brew install gradle || true
-brew install scala || true
-brew install sbt || true
-brew install typesafe-activator || true
-brew install google-app-engine || true
-brew install heroku-toolbelt || true
-brew install nodebrew || true
-brew install brew-cask || true
+brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install wget
+brew install curl
+brew install git
+brew install python3
+brew install openssl
+brew install readline
+brew install rbenv
+brew install ruby-build
+brew install php71
+brew install groovy
+brew install gradle
+brew install maven
+brew install scala
+brew install sbt
+brew install typesafe-activator
+brew install google-app-engine
+brew install heroku-toolbelt
+brew install nodebrew
 
-brew cleanup || true
+brew cleanup
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+brew cask install mi; brew cask update mi
+brew cask install java6; brew cask update java6
+brew cask install java7; brew cask update java7
+brew cask install java; brew cask update java
+brew cask install docker; brew cask update docker
+brew cask install google-cloud-sdk; brew cask update google-cloud-sdk
+brew cask install iterm2; brew cask update iterm2
+brew cask install emacs; brew cask update emacs
+brew cask install virtualbox; brew cask update virtualbox
+brew cask install karabiner; brew cask update karabiner
+brew cask install intellij-idea; brew cask update intellij-idea
+brew cask install googleappengine; brew cask update googleappengine
+brew cask install filezilla; brew cask update filezilla
+brew cask install duet; brew cask update duet
 
-brew cask install mi || true
-brew cask install java6
-brew cask install java7
-brew cask install java
-brew cask install google-cloud-sdk || true
-brew cask install iterm2 || true
-brew cask install emacs || true
-brew cask install virtualbox || true
-brew cask install karabiner || true
-brew cask install intellij-idea || true
-brew cask install googleappengine || true
-brew cask install filezilla || true
-brew cask install docker-machine || true
+brew cask install cocoapacketanalyzer; brew cask update cocoapacketanalyzer
 
-brew cask install cocoapacketanalyzer || true
+brew cask install google-chrome; brew cask update google-chrome
+brew cask install firefox-ja; brew cask update firefox-ja
+brew cask install skype; brew cask update skype
+brew cask install adobe-reader; brew cask update adobe-reader
+brew cask install libreoffice; brew cask update libreoffice
+brew cask install gimp; brew cask update gimp
+brew cask install xmind; brew cask update xmind
+brew cask install miro; brew cask update miro
+#brew cask install asepsis; brew cask update asepsis
+brew cask install kindle; brew cask update kindle
+#brew cask install send-to-kindle; brew cask update send-to-kindle
+brew cask install fitbit-connect; brew cask update fitbit-connect
 
-brew cask install google-chrome || true
-brew cask install firefox-ja || true
-brew cask install skype || true
-brew cask install adobe-reader || true
-brew cask install openoffice || true
-brew cask install gimp || true
-brew cask install xmind || true
-brew cask install miro || true
-brew cask install slack || true
-brew cask install asepsis || true
-brew cask install kindle || true
-brew cask install fitbit-connect || true
-
-brew cask cleanup || true
+brew cask cleanup
